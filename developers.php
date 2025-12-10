56b22d58020390a81fab7dd4f8c4528cb8bd3435
sheldon34 <mubokasheldonmuboka@gmail.com>
2025-12-10 07:42:09 +0300

    our assignment

---
+++ developers.php
@@ -0,0 +1,50 @@
+<?php include('header_dashboard.php'); ?>
+    <body id="class_div">
+		<?php include('navbar_about.php'); ?>
+        <div class="container-fluid">
+            <div class="row-fluid">
+                <div class="span12" id="content">
+                     <div class="row-fluid">
+                        <!-- block -->
+                        <div class="block">
+								<div class="navbar navbar-inner block-header">
+									<div id="" class="muted pull-right"><a href="index.php"><i class="icon-arrow-left"></i> Back</a></div>
+								</div>
+                            <div class="block-content collapse in">
+							<h3>Developers</h3>
+							<hr>
+							<img src="passportphoto.jpg" width="2407" height="2975" alt=""/>
+                                <div class="span3">
+										<center>
+										  <hr>
+										<p>Name: Bogere Ayub</p>
+										<p>Address: Kampala</p>
+										<p>Email: ayutide@gmail.com</p>
+										<p>Position: Programmer</p>
+								  </center>
+							  </div>
+                                <div class="span3">
+								  <center>
+								    <hr>
+													  <p>&nbsp;</p>
+								</center>
+								</div>
+                                <div class="span3">
+															<center>
+															  <hr>
+												<p>&nbsp;</p>
+								</center>
+								</div>
+                                <div class="span3">
+															<center>
+								  </center>
+                                </div>
+                            </div>
+                        </div>
+                     </div>
+                </div>
+            </div>
+        </div>
+		<?php include('script.php'); ?>
+    </body>
+</html>
---
+++ footer.php
@@ -0,0 +1,8 @@
+<center>
+		<footer>
+		
+		<p>OOPS Academy M-Learning Copyright 2025</p>
+			<!-- <p>Programmed by: John Kevin Lorayna BSIS 4-A</p> -->
+		</footer>
+</center>
+
